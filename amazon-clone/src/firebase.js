69f9324930e848ffbbd566ed6c5f66c4b475ab30
bkuscu <bkuscu@techfak.de>
2021-06-04 02:38:06 +0200

    initialized firebase, added a header navbar, added material UI for icons, navbar is resposive and sticky

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBb1FINoWAaTMBwtrBoalTORWv8EwUdM1w",
+    authDomain: "clone-67c99.firebaseapp.com",
+    projectId: "clone-67c99",
+    storageBucket: "clone-67c99.appspot.com",
+    messagingSenderId: "740554840136",
+    appId: "1:740554840136:web:ce1ad60f9ba6c0d392e07d",
+    measurementId: "G-NSTQNVDDS9"
+};
