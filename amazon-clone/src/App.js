8d767284f15d42be6c97d143b1b4dd4276ff5e90
bkuscu <bkuscu@techfak.de>
2021-06-05 01:15:31 +0200

    user authentication, login, registration works

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+
+
 
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //will only run once when the appr compoonent loads...
+    auth.onAuthStateChanged(authUser => {
+      console.log("The user is: ", authUser);
+
+      if(authUser) {
+        //the user just logged in / the user was logged in
+        dispatch({
+          type:"SET_USER",
+          user: authUser
+        })
+      } else {
+        //the user ist logged out
+        dispatch({
+          type:"SET_USER",
+          user: null
+        })
+      }
+    })
+  }, [])
+
   return (
     <Router>
       <div className="App">
--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBb1FINoWAaTMBwtrBoalTORWv8EwUdM1w",
     authDomain: "clone-67c99.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
     appId: "1:740554840136:web:ce1ad60f9ba6c0d392e07d",
     measurementId: "G-NSTQNVDDS9"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth }
