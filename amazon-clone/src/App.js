666f2e5293678f0078ea8c0a73d13f2fba5587c2
bkuscu <bkuscu@techfak.de>
2021-06-04 22:01:26 +0200

    finished the basket functionality; adding and removing items works

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,20 +3,20 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Switch>
-        <Route path="/checkout">
-            <h1>CHECKOUT</h1>
+          <Route path="/checkout">
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
-          
         </Switch>
       </div>
     </Router>
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
-function header() {
+function Header() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="https://onlinebusinessmanager.com/wp-content/uploads/2018/09/white-amazon-logo-png-6.png"
-      ></img>
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://onlinebusinessmanager.com/wp-content/uploads/2018/09/white-amazon-logo-png-6.png"
+        />
+      </Link>
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -31,17 +37,18 @@ function header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
-        
-        <div className="header__optionBasket">
+
+        <Link to="/checkout">
+          <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">0</span>
-        </div>
-
-
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
       </div>
-
     </div>
   );
 }
 
-export default header;
+export default Header;
